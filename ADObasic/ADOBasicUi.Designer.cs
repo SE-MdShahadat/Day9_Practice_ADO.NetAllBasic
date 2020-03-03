4314e0ceb794e35f7da552520315462c2e36ce80
SE-MdShahadat <se.mdshahadat@gmail.com>
2020-03-03 09:04:45 +0600

    Insert_feature_added

--- ADObasic/ADOBasicUi.Designer.cs
+++ ADObasic/ADOBasicUi.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.codeTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(183, 94);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 29);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.SaveButton);
+            this.groupBox1.Controls.Add(this.codeTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(274, 140);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Department Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name";
+            // 
+            // codeTextBox
+            // 
+            this.codeTextBox.Location = new System.Drawing.Point(90, 38);
+            this.codeTextBox.Name = "codeTextBox";
+            this.codeTextBox.Size = new System.Drawing.Size(168, 22);
+            this.codeTextBox.TabIndex = 1;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(90, 66);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(168, 22);
+            this.nameTextBox.TabIndex = 1;
             // 
             // ADOBasicUi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Name = "ADOBasicUi";
             this.Text = "ADO All Basic Practice";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox codeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ADObasic/ADOBasicUi.cs
+++ ADObasic/ADOBasicUi.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,70 @@ namespace ADObasic
         {
             InitializeComponent();
         }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string code = codeTextBox.Text;
+            string name = nameTextBox.Text;
+            Insert(code,name);
+        }
+        private void Insert(string code, string name)
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection();
+                string connectionString = @"Server=MDSHAHADAT; Database=StudentDB; Integrated Security=True;";
+                sqlConnection.ConnectionString = connectionString;
+
+                SqlCommand sqlCommand = new SqlCommand();
+                string commandString = @"Insert Into Departments (Code, Name) Values ('"+code+"', '"+name+"')";
+                sqlCommand.CommandText = commandString;
+
+                sqlCommand.Connection = sqlConnection;
+
+                sqlConnection.Open();
+                int isExecuted;
+                isExecuted= sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    MessageBox.Show("Information Saved Successfully!");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to Save");
+                }
+
+                sqlConnection.Close();
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+        //private void Insert()
+        //{
+        //    try
+        //    {
+        //        SqlConnection sqlConnection = new SqlConnection();
+        //        string connectionString = @"Server=MDSHAHADAT; Database=StudentDB; Integrated Security=True;";
+        //        sqlConnection.ConnectionString = connectionString;
+
+        //        SqlCommand sqlCommand = new SqlCommand();
+        //        string commandString = @"Insert Into Departments (Code, Name) Values ('CSE', 'Computer Science & Engineering')";
+        //        sqlCommand.CommandText = commandString;
+
+        //        sqlCommand.Connection = sqlConnection;
+
+        //        sqlConnection.Open();
+        //        sqlCommand.ExecuteNonQuery();
+        //        sqlConnection.Close();
+
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        MessageBox.Show(exception.Message);
+        //    }
+        //}
     }
 }
